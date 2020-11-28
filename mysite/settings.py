382d09551de4e16f230b1ca6384596d4662d919c
kidaqrus <adikwusule@gmail.com>
2020-11-28 12:53:03 +0000

    turned off debug.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cc*sk^7l)!#p7a(sy8+h10h1+(*kbzayt0!o#(%$p_z&vik_j2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.bitfastpay.org']
 
